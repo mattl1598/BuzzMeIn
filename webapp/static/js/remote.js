f5a1fa619b4f1de62d28b748c67f8fa1bb166163
Matthew Larby <mattl1598@gmail.com>
2021-07-17 13:13:17 +0100

    fixing git setup again

--- webapp/static/js/remote.js
+++ webapp/static/js/remote.js
@@ -6,35 +6,35 @@ const room = urlParams.get('room');
 
 socket.on('connect', function() {
 	socket.emit('test', {data: 'I\'m connected!'});
-    socket.emit('join', {"room": room, "client_type": "remote"})
+	socket.emit('join', {"room": room, "client_type": "remote"})
 });
 
 socket.on('ppAck', function ppAck(data) {
-   if (data["content"]) {
-       document.getElementById("pp").innerHTML = "Pause Game";
-   } else {
-       document.getElementById("pp").innerHTML = "Continue Game";
-   }
+	if (data["content"]) {
+		document.getElementById("pp").innerHTML = "Pause Game";
+	} else {
+		document.getElementById("pp").innerHTML = "Continue Game";
+	}
 });
 
 function play_pause() {
-    socket.emit('play_pause', {"room": room});
+	socket.emit('play_pause', {"room": room});
 }
 
 function restart() {
-    socket.emit("restart", {"room": room});
+	socket.emit("restart", {"room": room});
 }
 
 function send(msg) {
-    socket.emit('remote', {data: msg, "room": room});
+	socket.emit('remote', {data: msg, "room": room});
 }
 
 function arrow() {
-    if (event.srcElement.tagName === "path") {
-        console.log(event.srcElement.parentElement)
-        socket.emit('remote', {data: event.srcElement.parentElement.id, "room": room});
-    } else {
-        console.log(event.target.id)
-        socket.emit('remote', {data: event.target.id, "room": room});
-    }
+	if (event.srcElement.tagName === "path") {
+		console.log(event.srcElement.parentElement)
+		socket.emit('remote', {data: event.srcElement.parentElement.id, "room": room});
+	} else {
+		console.log(event.target.id)
+		socket.emit('remote', {data: event.target.id, "room": room});
+	}
 }
